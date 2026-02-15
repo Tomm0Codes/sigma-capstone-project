cadf49238aae5681db2509089826c49ecb8c86dc
Tomm0Codes <thomasking987@gmail.com>
2026-02-15 20:01:17 +0000

    Making the divergence test function for one complex number, changing the escape radius

--- mandelbrot.py
+++ mandelbrot.py
@@ -8,32 +8,43 @@ def main():
     c_real_range = np.linspace(-2, 1, num=50)
     c_imag_range = np.linspace(-1.5, 1.5, num=50)
     real, imag = np.meshgrid(c_real_range, c_imag_range)
-    c_range = real + imag*1j
     boundary = [
         c_real_range.min(), c_real_range.max(),
         c_imag_range.min(), c_imag_range.max()
     ]
-    magnitude = np.abs(c_range)
-    plt.imshow(magnitude, extent=boundary)
-    plt.show()
+    c = real + imag*1j
+    # look into applying function to whole array of c values
+
+    # plt.imshow(, extent=boundary)
+    # plt.show()
+
     # If it converges, it's within the set - no colour,
     # else it's filled with colour based on the time taken to diverge
-    divergence_time = divergence_test(c_range)
 
 
-def divergence_test(c_array: np.ndarray) -> np.ndarray:
+def divergence_test(c: complex) -> int:
     '''
-    This function takes the meshgrid of complex numbers and returns
-    0 if, in the Mandelbrot set formula, the number converges,
-    else the approximate time required for the number to diverge.
+    This function takes a complex number and tests if it converges
+    or diverges with repeated Mandelbrot iterations. It returns a count
+    of 0 if the number converges, or the count for how long the number takes
+    to diverge.
 
-    :param c: Array of test values for c
-    :type c: np.ndarray
-    :return: Array of time taken to converge/diverge
-    :rtype: ndarray[_AnyShape, dtype[Any]]
+    :param c: Complex number to test
+    :return: Iterations taken to converge/diverge
     '''
-    z = np.array([0])
+    z = 0
+    count = 0
+    while count < 20:
+        z = z**2 + c
+        print(z)
+        count += 1
+
+        # Escape radius is 2
+        if abs(z) >= 2:
+            return count
+
+    return 0
 
 
 if __name__ == '__main__':
-    main()
+    print(divergence_test(1j))
