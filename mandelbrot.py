38848df322f89d84de7244f34794d9a2589631ef
Tomm0Codes <thomasking987@gmail.com>
2026-02-18 00:05:26 +0000

    Starting on UI and method of keeping resolution as you zoom in

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,18 +1,21 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+import pyfiglet
 
 
 def main():
-    # Setup a "fractal generator UI"
-    # Get resolution from user
-    # Get type of fractal from user from pre list or one they want to try out
+    pass
+    # Setup a "fractal generator UI" that gets choice of visual
+    # (optional) and resolution for image
+    # Find a way to turn it into an animation that keeps the quality as you zoom in (main goal)
 
-    # Find a way to turn it into an animation that keeps the quality as you zoom in
-    # Setup my  complex equations plot
-    # If it converges, it's within the set - no colour,
-    # else it's filled with colour based on the time taken to diverge
-    resolution = resolution_input()
+
+def draw_me(img: np.ndarray):
+    ...
+
+
+def fractal_generator():
     c_real_range = np.linspace(-2, 1, num=1000)
     c_imag_range = np.linspace(-1.5, 1.5, num=1000)
     real, imag = np.meshgrid(c_real_range, c_imag_range)
@@ -21,16 +24,16 @@ def main():
         c_imag_range.min(), c_imag_range.max()
     ]
     c = real + imag*1j
-    vectorized_divergence_test = np.vectorize(divergence_test)
+    vectorized_mandelbrot_func = np.vectorize(mandelbrot_func)
     plt.imshow(
-        vectorized_divergence_test(c),
+        vectorized_mandelbrot_func(c),
         interpolation=None,
         extent=boundary
     )
     plt.show()
 
 
-def divergence_test(c: complex) -> int:
+def mandelbrot_func(c: complex) -> int:
     '''
     This function takes a complex number and tests if it converges
     or diverges with repeated Mandelbrot iterations. It returns a count
@@ -53,19 +56,36 @@ def divergence_test(c: complex) -> int:
     return 0
 
 
-def resolution_input() -> int:
+def user_integer_input(min: int, max: int, num_info: str) -> int:
     '''
-    This takes the users integer input between 0-300 for the DPI
-    resolution
+    This checks and returns the users integer input
     '''
     while True:
-        resolution = input('Enter a DPI resolution between 0-300: ')
-        if resolution.isdigit() and int(resolution) in range(1, 301):
-            return int(resolution)
+        user_num = input(f'{num_info} from {min}-{max}: ')
+        if user_num.isdigit() and int(user_num) in range(min, max):
+            return int(user_num)
 
         print('Invalid input, try again')
 
 
+def fractal_UI() -> tuple:
+    '''
+    This function gives the user an interactive interface to choose
+    a fractal (or make their own) and its resolution.
+    '''
+    banner = pyfiglet.figlet_format(
+        'Fractal Generator 1.0',
+        font='slant'
+    )
+    print(banner)
+    fractals = {
+        1: 'Mandelbrot Fractals',
+        2: 'Julia Sets',
+        3: 'Newton Fractals'
+    }
+    fractal_choice = user_integer_input(1, 3, 'Choose a fractal')
+    resolution = user_integer_input(1, 300, 'Enter a DPI resolution')
+
+
 if __name__ == '__main__':
-    resolution = resolution_input()
-    print(resolution)
+    main()
