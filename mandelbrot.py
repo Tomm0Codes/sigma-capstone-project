f7898131d74d27afd1ff4d35f4bb25795e5397a2
Tomm0Codes <thomasking987@gmail.com>
2026-02-19 02:33:51 +0000

    tidied other stuff, trying to fix zoom error where it shifts set out of place

--- mandelbrot.py
+++ mandelbrot.py
@@ -7,54 +7,62 @@ import sys
 
 def main():
     user_fractal_type, user_resolution = fractal_UI()
-    fractal_axes = {'Mandelbrot Set': np.array(((-2, 1), (-1.5, 1.5)))}
-    real_range, imag_range = fractal_axes[user_fractal_type]
+    fractal_axes = {'Mandelbrot Set': np.array((-2, 1, -1.5, 1.5))}
     fractal_gen_loop(
-        user_fractal_type, user_resolution, real_range, imag_range
+        user_fractal_type,
+        user_resolution,
+        *tuple(fractal_axes[user_fractal_type])
     )
 
 
 def fractal_gen_loop(
     fractal_type: str,
     resolution: int,
-    real_range: np.ndarray,
-    imag_range: np.ndarray,
+    real_min: int,
+    real_max: int,
+    imag_min: int,
+    imag_max: int
 ):
     """
     Loop to open fractal figure, wait for user click,
     close figure, then open again on new zoomed point.
     """
-    zoom = 1
+    zoom = 2
+    image_width = real_max - real_min
+    image_height = imag_max - imag_min
+
     while True:
-        fractal_generator(fractal_type, resolution, real_range, imag_range)
-        zoom *= 10
-        real_range = np.array(
-            ((real_coord - 1 / zoom), (real_coord + 1 / zoom))
+        fractal_generator(
+            fractal_type, resolution, real_min, real_max, imag_min, imag_max
         )
-        imag_range = np.array(
-            ((imag_coord - 1 / zoom), (imag_coord + 1 / zoom))
-        )
+        real_min = real_coord - (image_width / (2 * zoom))
+        real_max = real_coord + (image_width / (2 * zoom))
+        imag_min = imag_coord - (image_height / (2 * zoom))
+        imag_max = imag_coord + (image_width / (2 * zoom))
+        zoom *= zoom
 
 
 def fractal_generator(
     fractal_type: str,
     resolution: int,
-    real_range: np.ndarray,
-    imag_range: np.ndarray,
+    real_min: int,
+    real_max: int,
+    imag_min: int,
+    imag_max: int,
 ):
     """
     Generates a fractal image based on the axis range and resolution.
     """
     real, imag = np.meshgrid(
-        np.linspace(real_range[0], real_range[1], num=resolution),
-        np.linspace(imag_range[0], imag_range[1], num=resolution),
+        np.linspace(real_min, real_max, num=resolution),
+        np.linspace(imag_min, imag_max, num=resolution),
     )
 
     if fractal_type == 'Mandelbrot Set':
         c = real + imag * 1j
         vectorized_mandelbrot_func = np.vectorize(mandelbrot_func)
         mandelbrot_array = vectorized_mandelbrot_func(c)
-        boundary = [real_range[0], real_range[1], imag_range[0], imag_range[1]]
+        boundary = [real_min, real_max, imag_min, imag_max]
         plt.imshow(mandelbrot_array, interpolation=None, extent=boundary)
         plt.connect('button_press_event', on_click)
         plt.connect('key_press_event', on_press)
@@ -85,8 +93,8 @@ def mandelbrot_func(c: complex) -> int:
     """
     Takes a complex number and tests if it converges
     or diverges with repeated Mandelbrot iterations. It returns a count
-    of 0 if the number converges, or the count for how long the number takes
-    to diverge.
+    of 0 if the number converges, or the count of how many iterations it takes
+    for the initial number to diverge.
     """
     z = 0
 
@@ -107,11 +115,15 @@ def user_integer_input(lower: int, upper: int, s: str) -> int:
     This checks and returns the users integer input
     """
     while True:
-        user_num = input(s)
-        if user_num.isdigit() and int(user_num) in range(lower, upper + 1):
-            return int(user_num)
+        try:
+            user_input = int(input(s))
+            if user_input in range(lower, upper + 1):
+                return user_input
 
-        print('Invalid input, try again')
+            print('Out of range, try again')
+
+        except ValueError:
+            print('Invalid input, try again')
 
 
 def fractal_UI() -> tuple:
